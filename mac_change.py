7e267462e8175cb47dd3a60e56e34a7fac1b199d
Gautier FRANCHINI <gautier.franchini@data-essential.com>
2021-03-19 18:58:17 +0100

    Add Section 2: Using Conditional Statements in MAC Changer

--- mac_change.py
+++ mac_change.py
@@ -11,7 +11,14 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface",
                       help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error(
+            "[-] Please specify an interface, use --help for more info.")
+    if not options.new_mac:
+        parser.error(
+            "[-] Please specify a mac address, use --help for more info.")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -27,6 +34,6 @@ def print_interface_nfo(interface):
     subprocess.call("ifconfig " + interface, shell=True)
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
 print_interface_nfo(options.interface)
