cdc631f2362d4f28ff0a9e2433052a0993bba24c
Gautier FRANCHINI <gautier.franchini@data-essential.com>
2021-03-19 18:33:23 +0100

    Add Section 2: Writing a MAC Address Changer - Python Basics - 2nd draft

--- mac_change.py
+++ mac_change.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 
+# python3 mac_change.py -i eth0 -m 00: 11: 22: 33: 44: 55
+
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface", dest="interface",
-                  help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-(options, arguments) = parser.parse_args()
-
-# interface = input("interface > (e.g. wlan0)")
-# new_mac = input("new_mac > (e.g. 00:11:22:33:44:77 )")
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-# subprocess.call("ifconfig " + interface + " down", shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig " + interface + " up", shell=True)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return parser.parse_args()
 
 
-print("[+] printing ifconfig " + interface)
-subprocess.call("ifconfig " + interface, shell=True)
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
 
-# python3 mac_change.py - i eth0 - m 00: 11: 22: 33: 44: 55
+def print_interface_nfo(interface):
+    print("[+] printing ifconfig for interface: " + interface)
+    subprocess.call("ifconfig " + interface, shell=True)
+
+
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
+print_interface_nfo(options.interface)
